44777d91c858bbfa8f85078c93234b6bc98abe4e
N0ui <die_hecke@hotmail.de>
2015-12-23 01:25:45 +0100

    Add colors

--- pcp.js
+++ pcp.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name		pcp - pr0gramm color picker
-// @author		N0ui
-// @namespace	pcp
-// @include		http://*pr0gramm.com*
-// @include		https://*pr0gramm.com*
-// @version		0.1
-// @updateURL	https://raw.githubusercontent.com/N0ui/pr0gramm-color-picker/master/pcp.js
-// @downloadURL	https://raw.githubusercontent.com/N0ui/pr0gramm-color-picker/master/pcp.js
-// @copyright	2015+, N0ui
-// @description	alles bleibt hier nicht so wie es ist! (Für den der will)
-// @icon		http://pr0gramm.com/media/pr0gramm-favicon.png
-// @run-at       document-end
+// @name		      pcp - pr0gramm color picker
+// @author		      N0ui
+// @namespace	      pcp
+// @include		      http://*pr0gramm.com*
+// @include		      https://*pr0gramm.com*
+// @version		      0.1
+// @updateURL	      https://raw.githubusercontent.com/N0ui/pr0gramm-color-picker/master/pcp.js
+// @downloadURL	      https://raw.githubusercontent.com/N0ui/pr0gramm-color-picker/master/pcp.js
+// @copyright	      2015+, N0ui
+// @description	      alles bleibt hier nicht so wie es ist! (Für den der will)
+// @icon		      http://pr0gramm.com/media/pr0gramm-favicon.png
+// @run-at            document-end
 // ==/UserScript==
 (function () {
     'use strict';
@@ -20,6 +20,10 @@
             desc: 'Hauptfarbe',
             key: 'main-color',
             value: '#ee4d2e'
+        }, {
+            desc: 'Standard Schriftfarbe',
+            key: 'main-font-color',
+            value: '#f2f5f4'
         }, {
             desc: 'Linkfarbe',
             key: 'link-color',
@@ -48,18 +52,23 @@
             desc: 'Button Hover Text',
             key: 'btn-hover-color',
             value: '#555555'
+        }, {
+            desc: 'Tag Hintergrund',
+            key: 'tag-bg-color',
+            value: '#2A2E31'
         }],
 
         // style
         // style
         cssStyle: function () {
             var $styleEl = $('#pcp-style'),
-                cssStr = '';
+                cssStr = 'html, body, h3, .tab-bar a, .head-link, a#inboxLink, #inboxLink.empty, #search-submit-inline, .user {color: ' + localStorage["main-font-color"] + ';}';
+            cssStr += 'input.box-from-label:checked + label:before {background-color: ' + localStorage["main-font-color"] + ';}input.box-from-label + label:before {border: 1px solid ' + localStorage["main-font-color"] + ';}';
             cssStr += '#filter-save,.confirm-button, input[type=button], input[type=submit],.filter-setting.active .filter-check, .loader > div {';
             cssStr += 'background-color: ' + localStorage["main-color"] + ';}';
-            cssStr += '.filter-setting.active .filter-check {';
+            cssStr += '.filter-setting.active .filter-check, #upload-droparea {';
             cssStr += 'border: 1px solid ' + localStorage["main-color"] + ';}';
-            cssStr += 'a.bookmarklet, #settings-logout-link,.action,.filter-setting.active .filter-name,.head-link:hover,.tab-bar a:hover, .tab-bar a.active,a.head-tab.active, a.head-tab:hover,#inboxLink, #inboxLink.empty:hover,.head-link:hover {';
+            cssStr += 'a.bookmarklet, #settings-logout-link,.action,.filter-setting.active .filter-name,.head-link:hover,.tab-bar a:hover, .tab-bar a.active,a.head-tab.active, a.head-tab:hover,#inboxLink, #inboxLink.empty:hover,.head-link:hover, div.tagsinput span.tag a {';
             cssStr += 'color: ' + localStorage["main-color"] + ';}';
             cssStr += 'a, .link {';
             cssStr += 'color: ' + localStorage["link-color"] + ';}';
@@ -72,7 +81,7 @@
             cssStr += 'background-color: ' + localStorage["cancelBtn-bg-color"] + ';color: ' + localStorage["cancelBtn-color"] + ';}';
             cssStr += '.confirm-button:hover, input[type=button]:hover, input[type=button]:focus, input[type=submit]:hover, input[type=submit]:focus {';
             cssStr += 'background-color: ' + localStorage["btn-hover-bg-color"] + '; color:  ' + localStorage["btn-hover-color"] + ';}';
-            cssStr += 'background-color: ' + localStorage["btn-hover-bg-color"] + '; color:  ' + localStorage["btn-hover-color"] + ';}';
+            cssStr += 'span.tag {background-color: ' + localStorage["tag-bg-color"] + ' !important;}';
             
             cssStr += '.pcp-input-outer {display:block;margin: 0 0 10px 0;}.pcp-label {width: 40%; display:inline-block !important;}.pcp-color{padding: 0;width: 50px;display: inline-block;}#pcp-reset {border: 1px solid #fff;display: inline-block;padding: 8px 20px;cursor:pointer;}';
 
